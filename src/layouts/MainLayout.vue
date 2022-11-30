be6d40bcc6b698cea36f8842a8d045e5c60b8b73
Gustavo Lopez <20193tn081@utez.edu.mx>
2022-11-29 23:22:05 -0600

    feat: Added layout and more views

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,61 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr fFf">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title> CODE </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <div class="absolute-top bg-primary" style="height: 160px">
+        <div class="bg-transparent absolute full-width">
+          <q-avatar
+            class="absolute-center"
+            size="80px"
+            font-size="50"
+            color="orange-13"
+            text-color="white"
+          >
+            {{ initials }}
+          </q-avatar>
+          <div
+            class="text-weight-bold text-center text-white text-h6"
+            style="margin-top: 120px"
+          >
+            <router-link
+              style="color: inherit; text-decoration-color: initial"
+              :to="{ name: 'profile' }"
+            >
+              {{ fullName }}
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <q-scroll-area style="height: calc(100% - 160px); margin-top: 160px">
+        <q-list padding>
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+          />
+        </q-list>
+      </q-scroll-area>
+      <div class="absolute-bottom" style="height: 100px">
+        <div class="bg-transparent full-width">
+          <q-btn
+            style="width: 80%"
+            color="primary"
+            class="absolute-center"
+            icon="logout"
+            label="Cerrar sesión"
+            glossy
+            unelevated
+            rounded
+            @click="logoutPage()"
+          ></q-btn>
+        </div>
+      </div>
     </q-drawer>
 
     <q-page-container>
@@ -46,71 +65,62 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Perfil",
+    caption: "",
+    icon: "account_box",
+    link: { name: "profile" },
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Donaciones",
+    caption: "",
+    icon: "volunteer_activism",
+    link: "",
+    sub_items: [
+      {
+        title: "Pendientes",
+        caption: "",
+        icon: "pending_actions",
+        link: { name: "donations" },
+      },
+      {
+        title: "Realizadas",
+        caption: "",
+        icon: "assignment_turned_in",
+        link: { name: "donations" },
+      },
+    ],
   },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+];
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
-    EssentialLink
+    EssentialLink,
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
+      fullName: ref(""),
+      initials: ref(""),
       essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+  methods: {
+
+  },
+  mounted() {
+  },
+});
 </script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,100 @@
+<template>
+  <img src="../assets/wave.png" class="wave" alt="login-wave" />
+  <div class="row" style="height: 90vh">
+    <div class="col-0 col-md-6 flex justify-center content-center">
+      <img
+        src="../assets/cesta-de-comida.png"
+        class="responsive"
+        alt="login-image"
+      />
+    </div>
+    <div
+      v-bind:class="{
+        'justify-center': $q.screen.md || $q.screen.sm || $q.screen.xs,
+      }"
+      class="col-12 col-md-6 flex content-center"
+    >
+      <q-card
+        v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '50%' }"
+      >
+        <q-card-section>
+          <q-avatar size="103px" class="absolute-center shadow-10">
+            <img
+              size="100px"
+              src="https://cdn.quasar.dev/logo-v2/svg/logo.svg"
+              alt="avatar"
+            />
+          </q-avatar>
+        </q-card-section>
+        <q-card-section>
+          <div class="q-pt-lg">
+            <div class="col text-h6 ellipsis flex justify-center">
+              <h2 class="text-h3 text-uppercase q-my-none text-weight-regular">
+                Login
+              </h2>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          <q-form class="q-gutter-md">
+            <q-input
+              label="Username"
+              rounded
+              outlined
+              dense
+              v-model="user.email"
+            >
+            </q-input>
+            <q-input
+              label="Password"
+              rounded
+              dense
+              outlined
+              type="password"
+              v-model="user.password"
+            >
+            </q-input>
+            <div>
+              <q-btn
+                class="full-width"
+                color="primary"
+                label="Login"
+                rounded
+              ></q-btn>
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  name: "LoginPage",
+  setup() {
+    return {
+      user: reactive({
+        email: ref(""),
+        password: ref(""),
+        device_name: ref(navigator.userAgent),
+      }),
+    };
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+.wave {
+  position: fixed;
+  height: 100%;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+}
+</style>
