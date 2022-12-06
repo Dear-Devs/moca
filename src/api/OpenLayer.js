4078936a9c010bf877a166b9d37ed89a4403c1d6
Gustavo Lopez <thered04@outlook.com>
2022-12-06 11:11:48 -0600

    feat: added geolocation in map

--- src/api/OpenLayer.js
+++ src/api/OpenLayer.js
@@ -1,37 +1,77 @@
-import Feature from "ol/Feature";
-import Point from "ol/geom/Point";
-import Icon from "ol/style/Icon";
-import Style from "ol/style/Style";
-import VectorLayer from "ol/layer/Vector";
-import VectorSource from "ol/source/Vector";
-import View from "ol/View";
-import Map from "ol/Map";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import * as olProj from "ol/proj";
+import Feature from 'ol/Feature';
+import Geolocation from 'ol/Geolocation';
+import Map from 'ol/Map';
+import Point from 'ol/geom/Point';
+import View from 'ol/View';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
 // importing the OpenLayers stylesheet is required for having
 // good looking buttons!
 import "ol/ol.css";
 
 const createMap = (reference, latitude, longitude) => {
-  return new Map({
-    // the map will be created using the 'map-root' ref
-    target: reference,
-    layers: [
-      // adding a background tiled layer
-      new TileLayer({
-        source: new OSM(), // tiles are served by OpenStreetMap
-      }),
-    ],
 
-    // the map view will initially show the whole world
-    view: new View({
-      zoom: 15,
-      center: olProj.fromLonLat([longitude, latitude]),
-      constrainResolution: true,
+const view = new View({
+  center: [longitude, latitude],
+  zoom: 10,
+});
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
     }),
-  });
+  ],
+  target: reference,
+  view: view,
+});
+
+const geolocation = new Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true,
+  },
+  projection: view.getProjection(),
+});
+
+  geolocation.setTracking(true);
+
+
+
+const accuracyFeature = new Feature();
+geolocation.on('change:accuracyGeometry', function () {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+const positionFeature = new Feature();
+positionFeature.setStyle(
+  new Style({
+    image: new CircleStyle({
+      radius: 7,
+      fill: new Fill({
+        color: '#FF0000',
+      }),
+      stroke: new Stroke({
+        color: '#fff',
+        width: 2,
+      }),
+    }),
+  })
+);
+
+geolocation.on('change:position', function () {
+  const coordinates = geolocation.getPosition();
+  positionFeature.setGeometry(coordinates ? new Point(coordinates) : null);
+});
+
+return new VectorLayer({
+  map: map,
+  source: new VectorSource({
+    features: [accuracyFeature, positionFeature],
+  }),
+});
 };
 
 export { createMap };
