e85570420c1766c76e3d97d522bdf1894a94c48a
Gustavo Lopez <thered04@outlook.com>
2022-12-06 02:23:17 -0600

    feat: Added map

--- src/pages/CheckInputOutput.vue
+++ src/pages/CheckInputOutput.vue
@@ -1,55 +1,208 @@
 <template>
-   <q-page>
-    <div class="flex justify-around items-center q-mt-md">
-      <q-uploader
-        url="http://localhost:4444/upload"
-        style="max-width: 320px"
-      />
-
-      <q-img
-        src="https://placeimg.com/500/300/nature"
-        style="max-width: 320px"
-        class="q-mt-md"
-      />
-    </div>
-
-    <div class="q-mt-md text-center">Hora de entrada y salida</div>
-    <div class="q-mt-md text-center">8 AM</div>
-
-    <div class="flex justify-around">
-      <q-btn
-        class="q-mt-md"
-        color="primary"
-        label="Regresar"
-        icon="keyboard_return"
-        glossy
-        unelevated
-        rounded
-      />
+  <q-page>
+    <div class="main">
+      <div class="flex">
+        <!-- Map Display here -->
+        <div class="map-holder">
+          <div id="map"></div>
+        </div>
+        <!-- Coordinates Display here -->
+        <div class="dislpay-arena">
+          <div class="coordinates-header">
+            <h3>Current Coordinates</h3>
+            <p>Latitude: {{ center[0] }}</p>
+            <p>Longitude: {{ center[1] }}</p>
+          </div>
+          <div class="coordinates-header">
+            <h3>Current Location</h3>
+            <div class="form-group">
+              <input
+                type="text"
+                class="location-control"
+                :value="location"
+                readonly
+              />
+              <button type="button" class="copy-btn">Copy</button>
+            </div>
+            <button type="button" class="location-btn">Get Location</button>
+          </div>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import axios from "axios";
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default {
   name: "CheckInputOutput",
   setup() {
     return {
-      user: reactive({
-        email: ref(""),
-        password: ref(""),
-      }),
+      loading: false,
+      location: "",
+      access_token:
+        "sk.eyJ1IjoiaTIwMTkzdG4xMjgiLCJhIjoiY2xiYjV2aTlwMDJjdTNxcGMzaHpsYWdlYyJ9.afpg0G50-algaY6W78cJzg",
+      center: [10, 0],
+      map: {},
     };
   },
 
   methods: {
+    async createMap() {
+      try {
+        mapboxgl.accessToken = this.access_token;
+        this.map = new mapboxgl.Map({
+          container: "map",
+          style: "https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.css",
+          center: this.center,
+          zoom: 11,
+        });
+
+        let geocoder = new MapboxGeocoder({
+          accessToken: this.access_token,
+          mapboxgl: mapboxgl,
+          marker: false,
+        });
+
+        this.map.addControl(geocoder);
+
+        geocoder.on("result", (e) => {
+          const marker = new mapboxgl.Marker({
+            draggable: true,
+            color: "#D80739",
+          })
+            .setLngLat(e.result.center)
+            .addTo(this.map);
+          this.center = e.result.center;
+          marker.on("dragend", (e) => {
+            this.center = Object.values(e.target.getLngLat());
+          });
+        });
+      } catch (err) {
+        console.log("map error", err);
+      }
+    },
+
+    async getLocation() {
+      try {
+        this.loading = true;
+        const response = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${this.center[0]},${this.center[1]}.json?access_token=${this.access_token}`
+        );
+        console.log(response);
+        this.loading = false;
+        this.location = response.data.features[0].place_name;
+      } catch (err) {
+        this.loading = false;
+        console.log(err);
+      }
+    },
+
+    copyLocation() {
+      if (this.location) {
+        navigator.clipboard.writeText(this.location);
+        alert("Location Copied");
+      }
+      return;
+    },
+  },
+
+  mounted() {
+    this.createMap();
   },
 };
 </script>
 
 <style scoped>
+.main {
+  padding: 45px 50px;
+}
+.flex {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.map-holder {
+  width: 65%;
+}
+#map {
+  height: 70vh;
+}
+.dislpay-arena {
+  background: #ffffff;
+  box-shadow: 0px -3px 10px rgba(0, 58, 78, 0.1);
+  border-radius: 5px;
+  padding: 20px 30px;
+  width: 25%;
+}
+.coordinates-header {
+  margin-bottom: 50px;
+}
+.coordinates-header h3 {
+  color: #1f2a53;
+  font-weight: 600;
+}
+.coordinates-header p {
+  color: rgba(13, 16, 27, 0.75);
+  font-weight: 600;
+  font-size: 0.875rem;
+}
+.form-group {
+  position: relative;
+}
+.location-control {
+  height: 30px;
+  background: #ffffff;
+  border: 1px solid rgba(31, 42, 83, 0.25);
+  box-shadow: 0px 0px 10px rgba(73, 165, 198, 0.1);
+  border-radius: 4px;
+  padding: 0px 10px;
+  width: 90%;
+}
+.location-control:focus {
+  outline: none;
+}
+.location-btn {
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: #d80739;
+  box-shadow: 0px 4px 10px rgba(73, 165, 198, 0.1);
+  border-radius: 5px;
+  border: 0;
+  cursor: pointer;
+  color: #ffffff;
+  font-size: 0.875rem;
+  font-weight: 600;
+}
+.location-btn:focus {
+  outline: none;
+}
+.disabled {
+  background: #db7990;
+  cursor: not-allowed;
+}
+.copy-btn {
+  background: #f4f6f8 0% 0% no-repeat padding-box;
+  border: 1px solid #f4f6f8;
+  border-radius: 0px 3px 3px 0px;
+  position: absolute;
+  color: #5171ef;
+  font-size: 0.875rem;
+  font-weight: 500;
+  height: 30px;
+  padding: 0px 10px;
+  cursor: pointer;
+  right: 3.5%;
+  top: 5%;
+}
+.copy-btn:focus {
+  outline: none;
+}
 .wave {
   position: fixed;
   height: 100%;
